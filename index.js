5740d48d46d1554c37325f6aaf38f9e6e0509697
Thach Le <levu48@gmail.com>
2017-09-09 10:25:49 -0700

    initial

---
+++ index.js
@@ -0,0 +1,2 @@
+import {withInstance, instanceState, instanceReducer, instanceAction} from './instance';
+export {withInstance, instanceState, instanceReducer, instanceAction};
---
+++ instance.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {v4} from 'uuid';
+import {injectReducer} from './dynamix';
+
+export const withInstance = (ComposedComponent) => {
+    return class withInstance extends React.Component {
+        static propTypes = {
+            __reducer: PropTypes.func
+        }
+
+        static defaultProps = {
+            __reducer: (state, action) => ({...state, '__name': 'default withInstance reducer'})
+        }
+
+        constructor(props) {
+            super(props);
+            this.__id = ComposedComponent.displayName + "/" + v4();
+            //injectReducer(ComposedComponent.displayName + "/" + this.__id, props.__reducer);
+            injectReducer(this.__id, props.__reducer);
+
+        }
+        render() {
+            return <ComposedComponent __id={this.__id} {...this.props} />;
+        }
+    }
+}
+
+// export const instanceActions = (Actions, props) => {
+//     let obj = {};
+//     Object.keys(Actions).forEach(key => {
+//         obj[key] = {...Actions[key], __id: props.__id};
+//     });
+//     return obj;
+// }
+
+export const instanceAction = (action, props) => {
+    return {...action, __id: props.__id}
+}
+
+export const instanceState = (func) => (state, props) => {
+    return func(state[props.__id], props);
+}
+
+export const instanceReducer = (reducer) => (state, action) => {
+    if (!isInstance(state, action)) { 
+        return state;
+    }
+    return reducer(state, action);
+}
+
+export const isInstance = (state = {}, action = {}) => (
+    state.__id && action.__id && state.__id === action.__id
+        ? true : false
+)
